f930f5620b5a74ab1358656c5b85d12b29729038
Looka997 <luca.marinelli997@gmail.com>
2021-06-30 17:34:18 +0200

    separates worker from listener

--- src/main/java/client/RegisterListener.java
+++ src/main/java/client/RegisterListener.java
@@ -1,12 +1,10 @@
 package client;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RegisterListener extends SwingWorker<Boolean, Boolean> implements ActionListener {
-    private Container pane;
+public class RegisterListener implements ActionListener {
     private JTextField plate;
     private JComboBox brand;
 
@@ -17,55 +15,12 @@ public class RegisterListener extends SwingWorker<Boolean, Boolean> implements A
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        this.pane = ((JButton) e.getSource()).getParent();
-        this.execute();
-    }
-        @Override
-    protected Boolean doInBackground() throws Exception {
-        String pl_str = plate.getText();
-        String br_str = (String) brand.getSelectedItem();
-        System.out.println(pl_str);
-        System.out.println(br_str);
-        return null;
-    }
-
-    @Override
-    protected void done() {
-        JButton enterButton, exitButton;
-        pane.removeAll();
-
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weightx = 0.5;
-        JTextArea textArea = new JTextArea(10,40);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.insets = new Insets(24,30,24,30);
-        textArea.setEditable(false);
-        pane.add(textArea, c);
-
-        enterButton = new JButton("Enter");
-        c.gridy = 1;
-        c.gridx = 0;
-        c.ipady = 10;   // y padding for both buttons
-        c.ipadx = 7;    // x padding for both buttons
-        c.insets = new Insets(0,16,0,8);
-        pane.add(enterButton, c);
-
-        JTextField textField = new JTextField(6);
-        c.gridx = 1;
-        c.insets = new Insets(0,8,0,16);
-        pane.add(textField,c);
-
-        exitButton = new JButton("Exit");
-        c.gridx = 0;
-        c.gridy = 2;
-        c.insets = new Insets(16,16,16,8);
-        pane.add(exitButton, c);
-        pane.requestFocusInWindow();
-        pane.repaint();
-        pane.validate();
-
-        enterButton.addActionListener(new EnterListener(enterButton, textField, exitButton));
+        String pl_str, br_str;
+        pl_str = plate.getText();
+        br_str = (String) brand.getSelectedItem();
+        if (pl_str.equals("") || br_str.equals(""))
+            return;
+        RegisterWorker worker = new RegisterWorker(plate.getParent());
+        worker.execute();
     }
 }
